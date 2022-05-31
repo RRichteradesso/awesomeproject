a87a5d92858c0b3ba04ab8e405309fa30f35fe62
adotras <torsten.raskob@adesso-orange.com>
2022-05-31 10:57:28 +0200

    Initial

---
+++ test.bat
@@ -0,0 +1,7 @@
+echo "# awesomeproject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/adotras/awesomeproject.git
+git push -u origin main
